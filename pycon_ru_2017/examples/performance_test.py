e23a5bfeb90f7443ef3c4fcf1c757d8d38ef151d
szypulka <szypulka@yandex-team.ru>
2017-07-17 13:01:52 +0300

    Logging added

--- pycon_ru_2017/examples/performance_test.py
+++ pycon_ru_2017/examples/performance_test.py
@@ -18,40 +18,44 @@ LOGGER.addHandler(FILE_HANDLER)
 
 
 class LoadTest(BaseLoadTest):
-    def __init__(self, gun):
 
+    def __init__(self, gun):
         super(LoadTest, self).__init__(gun)
         self.wait_for_activation = json.loads(gun.get_option('wait_for_activation', 'true'))
 
     @handle_exception
     def get_state(self):
+        LOGGER.debug('Request for cluster state')
         with self.gun.measure('get_state') as measure:
             response = self.client.get_state()
             if not response:
                 measure['proto_code'] = 404
-
+                LOGGER.warning('No cluster state received')
         return response
 
     @handle_exception
     def apply(self, cluster_state):
-
         configuration = parse_cluster_state(cluster_state)
+        LOGGER.debug('Apply configuration %s', configuration)
         with self.gun.measure('apply') as measure:
             self.client.apply(configuration)
+        return configuration
 
     @handle_exception
-    def check_state(self):
+    def check_state(self, configuration):
+        LOGGER.debug('Check state for new configuration')
         with self.gun.measure('check_state') as measure:
-            state = self.client.check_state()
+            state = self.client.check_state(configuration.id)
+        LOGGER.info('State for config_id %s is %s', configuration.id, state)
         return state
 
     def default(self):
         current_state = self.get_state()
         with self.gun.measure('activated'):
-            self.apply(current_state)
+            config = self.apply(current_state)
             if self.wait_for_activation:
                 counter = 0
                 state = False
                 while not state and counter < RETRY_LIMITS:
-                    state = self.check_state()
+                    state = self.check_state(config.id)
                     counter += 1
